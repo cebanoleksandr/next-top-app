d9260239514964ac564729a8fe753723065d87cb
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-12-09 12:34:59 +0200

    Add sidebar

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -1,11 +1,16 @@
 import Menu from "@/app/(site)/components/Menu";
 import { FC, HTMLAttributes } from "react";
+import cn from "classnames";
+import Image from "next/image";
+import logo from "../../public/logo.svg";
 
 interface IProps extends HTMLAttributes<HTMLElement> {}
 
 const Sidebar: FC<IProps> = ({ className, ...props }) => {
   return (
-    <aside className={`${className}`} {...props}>
+    <aside className={cn(className, 'grid content-start gap-5')} {...props}>
+      <Image src={logo} className="mt-8" alt="" />
+      <div>поиск</div>
       <Menu />
     </aside>
   );
