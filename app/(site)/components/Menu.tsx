603743829e256e9d61e8c3e6c2a7bec134a9aa1e
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-12-23 11:58:25 +0200

    Add Menu animation

--- app/(site)/components/Menu.tsx
+++ app/(site)/components/Menu.tsx
@@ -2,15 +2,14 @@
 
 import { getMenu } from "@/api/menu";
 import { FirstLevelMenuItem, PageItem } from "@/interfaces/menu.interface";
-import { TopLevelCategory } from "@/interfaces/page.interface";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { setMenuAC } from "@/store/menuSlice";
-import { AcademicCapIcon, BookOpenIcon, CloudIcon, Squares2X2Icon } from "@heroicons/react/16/solid";
 import Link from "next/link";
 import { useEffect } from "react";
 import cn from "classnames";
 import { usePathname } from "next/navigation";
 import { firstLevelMenu } from "@/helpers/helpers";
+import { motion } from "framer-motion";
 
 const Menu = () => {
   const { menu, firstCategory } = useAppSelector(state => state.menu);
@@ -37,6 +36,30 @@ const Menu = () => {
     fetchMenu();
   }, []);
 
+  const variants = {
+    visible: {
+      marginBottom: 20,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      }
+    },
+    hidden: {
+      marginBottom: 0,
+    },
+  };
+
+  const variantsChildren = {
+    visible: {
+      opacity: 1,
+      height: 'auto',
+    },
+    hidden: {
+      opacity: 0,
+      height: 0,
+    },
+  };
+
   const buildFirstLevel = () => {
     return (
       <>
@@ -62,7 +85,7 @@ const Menu = () => {
     return (
       <div className="ml-3 mt-4 pl-8 border-l border-l-gray-300">
         {menu.map(m => {
-          const isActive = m.pages.some(p => p.alias === pathname.split('/')[2]);
+          const isActive = m.pages.some(p => p.alias === pathname?.split('/')[2]);
 
           const newM = {
             ...m,
@@ -75,13 +98,15 @@ const Menu = () => {
                 {m._id.secondCategory}
               </div>
 
-              <div
-                className={cn('block mb-2 text-[var(--gray-dark)] text-sm font-medium', {
-                  'hidden': !newM.isOpened
-                })}
+              <motion.div
+                className={cn('block mb-2 text-[var(--gray-dark)] text-sm font-medium')}
+                layout
+                variants={variants}
+                initial={newM.isOpened ? 'visible' : 'hidden'}
+                animate={newM.isOpened ? 'visible' : 'hidden'}
               >
                 {buildThirdLevel(m.pages, menuItem.route)}
-              </div>
+              </motion.div>
             </div>
           )
         })}
@@ -93,15 +118,19 @@ const Menu = () => {
     return (
       <div className="">
         {pages.map(page => (
-          <Link
-            key={page._id} 
-            href={`/${route}/${page.alias}`}
-            className={cn('block hover:text-[var(--primary)] transition duration-300', {
-              'text-[var(--primary)]': `/${route}/${page.alias}` === pathname
-            })}
+          <motion.div
+            key={page._id}
+            variants={variantsChildren}
           >
-            {page.category}
-          </Link>
+            <Link
+              href={`/${route}/${page.alias}`}
+              className={cn('block hover:text-[var(--primary)] transition duration-300', {
+                'text-[var(--primary)]': `/${route}/${page.alias}` === pathname
+              })}
+            >
+              {page.category}
+            </Link>
+          </motion.div>
         ))}
       </div>
     );
